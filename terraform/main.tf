9449285ac716100d812a5f7ed674b7c4f5ea3aa7
Seifeddine Touj <seifeddine.touj@etudiant-fst.utm.tn>
2024-10-26 11:46:46 +0200

    Adding inbound rules to the created sg

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,37 +37,6 @@ resource "aws_security_group" "eks_cluster_sg" {
   }
 }
 
-resource "aws_security_group" "eks_worker_sg" {
-  name        = "eks-worker-sg-${var.cluster_name}"
-  description = "Security group for EKS worker nodes ${var.cluster_name}"
-  vpc_id      = var.vpc_id  # Utilisation de la variable pour l'ID du VPC
-
-  ingress {
-    from_port   = 8083
-    to_port     = 8083
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 30000
-    to_port     = 30000
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "eks-worker-sg-${var.cluster_name}"
-  }
-}
-
 resource "aws_eks_cluster" "my_cluster" {
   name     = var.cluster_name
   role_arn = var.role_arn
@@ -77,6 +46,28 @@ resource "aws_eks_cluster" "my_cluster" {
     subnet_ids         = var.subnet_ids
     security_group_ids = [aws_security_group.eks_cluster_sg.id]
   }
+}
+
+data "aws_eks_cluster" "existing" {
+  name = aws_eks_cluster.my_cluster.name
+}
+
+resource "aws_security_group_rule" "worker_port_8083" {
+  type              = "ingress"
+  from_port         = 8083
+  to_port           = 8083
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = data.aws_eks_cluster.existing.vpc_config[0].cluster_security_group_id
+}
+
+resource "aws_security_group_rule" "worker_port_30000" {
+  type              = "ingress"
+  from_port         = 30000
+  to_port           = 30000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = data.aws_eks_cluster.existing.vpc_config[0].cluster_security_group_id
 }
 
 resource "aws_eks_node_group" "my_node_group" {
